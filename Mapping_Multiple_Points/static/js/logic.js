49292bb7a5a66c0798380775be0b34b69aefb5fd
Hitha Das <hitha.nair9@gmail.com>
2022-12-04 18:56:36 -0600

    Multiple Point Mapping with circle marker

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -12,17 +12,16 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
-//  Add a marker to the map for Los Angeles, California.
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-// Add circle marker
-// L.circle([34.0522, -118.2437], {
-   //  radius: 100
- // }).addTo(map);
-
- // Add a circleMarker function
- L.circleMarker([34.0522, -118.2437],{
- radius:300,
- color: "black",
- fillColor:'#ffffa1'
-}).addTo(map);
+// Get data from cities.js
+let cityData = cities;
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location, {
+    radius: city.population/200000,
+    color : "orange",
+    fillColor:'#ffffa1'
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+ });
